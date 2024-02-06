2a858d9ebc1571b929c4e83dca21c45c01ee6eca
mpavon17 <spavon17@gmail.com>
2024-02-06 21:05:28 +0100

    ee

--- app/src/main/java/com/example/pruebafirebase/MainActivity2.java
+++ app/src/main/java/com/example/pruebafirebase/MainActivity2.java
@@ -13,25 +13,11 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.pruebafirebase.databinding.ActivityMain2Binding;
 
 public class MainActivity2 extends AppCompatActivity {
-
-    private ActivityMain2Binding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main2);
     }
 
 }
